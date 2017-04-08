de9b0009860c5f268e0dff3fb95943d538c63b05
DanewDr <danen1701@gmail.com>
2017-04-09 07:47:41 +1000

    Add files via upload

---
+++ CookieMod/Items/Placeable/CrunchWood.cs
@@ -0,0 +1,26 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CookieMod.Items.Placeable
+{
+	public class ChrunchWood : ModItem
+	{
+		public override void SetDefaults()
+		{
+			item.name = "Chrunchwood";
+			item.width = 12;
+			item.height = 12;
+			item.maxStack = 999;
+			AddTooltip("Don't know why you'd think to eat it.");
+			item.rare = 2;
+			item.useTurn = true;
+			item.autoReuse = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.useStyle = 1;
+			item.consumable = true;
+			item.createTile = mod.TileType("CrunchWoodTile");
+		}
+	}
+}
