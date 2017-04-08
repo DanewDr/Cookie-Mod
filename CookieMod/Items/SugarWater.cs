0c8913b8f628a99d54baea998b1dca0549a399c8
DanewDr <danen1701@gmail.com>
2017-04-08 10:50:10 +1000

    Add files via upload

---
+++ CookieMod/Items/SugarWater.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CookieMod.Items
+{
+    public class SugarWater : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.CloneDefaults(ItemID.LesserHealingPotion);
+            item.name = "Sugar Water";
+        }
+    }
+}
