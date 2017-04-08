1f29f8d2b929c5234edba77993af41996fb3a702
DanewDr <danen1701@gmail.com>
2017-04-09 08:22:31 +1000

    Add files via upload

---
+++ CookieMod/Items/Placeable/CrunchWoodDresser.cs
@@ -0,0 +1,34 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CookieMod.Items.Placeable
+{
+	public class CrunchWoodDresser: ModItem
+	{
+		public override void SetDefaults()
+		{
+			item.name = "Crunch Dresser";
+			item.width = 26;
+			item.height = 22;
+			item.maxStack = 99;
+			AddTooltip("Change your clothes and even store items!");
+			item.useTurn = true;
+			item.autoReuse = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.useStyle = 1;
+			item.consumable = true;
+			item.value = 500;
+			item.createTile = mod.TileType(CrunchWoodDresser");
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Dresser);
+			recipe.AddIngredient(null, "CrunchWood", 10);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
