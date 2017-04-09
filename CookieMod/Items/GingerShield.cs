b534d895004118d8b4d1b6252976d36dade9df77
CookieBiscuit <aidan.n.borne@gmail.com>
2017-04-09 10:05:47 -0400

    Update GingerShield.cs

--- CookieMod/Items/GingerShield.cs
+++ CookieMod/Items/GingerShield.cs
@@ -20,7 +20,7 @@ namespace CookieMod.Items     //We need this to basically indicate the folder wh
             item.width = 24;   //The size in width of the sprite in pixels.
             item.height = 28;    //The size in height of the sprite in pixels.
             item.toolTip = "Grants immunity to knockback.";
-			item.toolTip2 = "Defence increased by 2";	//The description of the Weapon shown when hovering over the item ingame.
+	    item.toolTip2 = "8% melee damage increase";	//The description of the Weapon shown when hovering over the item ingame.
             item.value = Item.buyPrice(0, 2, 0, 0); //  How much the item is worth, in copper coins, when you sell it to a merchant. It costs 1/5th of this to buy it back from them. An easy way to remember the value is platinum, gold, silver, copper or PPGGSSCC (so this item price is 2 gold)
             item.rare = 4;          //The color the title of your Weapon when hovering over it ingame        
             item.accessory = true;  //this make the item an accessory, so you can equip it
@@ -29,7 +29,9 @@ namespace CookieMod.Items     //We need this to basically indicate the folder wh
         }
         public override void UpdateAccessory(Player player, bool hideVisual)  //this is so when the item is equipped will give this stats to the player
         {
-            player.noKnockback = true;   //so the player is immune to knockback 
+            player.noKnockback = true;   //so the player is immune to knockback
+	    player.meleeDamage += .08f;
+	    player.meleeSpeed += .04f;
         }
  
  
