d7125271932dba0f931f7ae36e42c55c917e36ec
DanewDr <danen1701@gmail.com>
2017-04-10 13:36:22 +1000

    Add files via upload

---
+++ CookieMod/NPCs/BunnyWarrior.cs
@@ -0,0 +1,40 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CookieMod.NPCs
+{
+    public class BunnyWarrior : ModNPC
+    {
+        public override void SetDefaults()
+        {
+            npc.name = "Bunny Warrior";
+            npc.displayName = "Bunny Warrior";
+            npc.width = 39;
+            npc.height = 34;
+            npc.damage = 10;
+            npc.defense = 10;
+            npc.lifeMax = 200;
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath13;
+            npc.value = 60f;
+            npc.knockBackResist = 0.5f;
+            npc.aiStyle = 1;
+            Main.npcFrameCount[npc.type] = 1; 
+            aiType = NPCID.Slime;  //npc behavior
+            animationType = NPCID.Slime;
+			npc.noGravity = true;
+        }
+
+        public override void FindFrame(int frameHeight)
+        {
+            npc.frameCounter -= 0.5F; // Determines the animation speed. Higher value = faster animation.
+            npc.frameCounter %= Main.npcFrameCount[npc.type];
+            int frame = (int)npc.frameCounter;
+            npc.frame.Y = frame * frameHeight;
+
+            npc.spriteDirection = npc.direction;
+        }
+    }
+}
---
+++ CookieMod/NPCs/GoldenBunnyWarrior.cs
@@ -0,0 +1,40 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CookieMod.NPCs
+{
+    public class GoldenBunnyWarrior : ModNPC
+    {
+        public override void SetDefaults()
+        {
+            npc.name = "Golden Bunnt Warrior";
+            npc.displayName = "Golden Bunnt Warrior";
+            npc.width = 39;
+            npc.height = 34;
+            npc.damage = 10;
+            npc.defense = 10;
+            npc.lifeMax = 200;
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath13;
+            npc.value = 60f;
+            npc.knockBackResist = 0.5f;
+            npc.aiStyle = 26;
+            Main.npcFrameCount[npc.type] = 1; 
+            aiType = NPCID.Unicorn;  //npc behavior
+            animationType = NPCID.Unicorn;
+			npc.noGravity = true;
+        }
+
+        public override void FindFrame(int frameHeight)
+        {
+            npc.frameCounter -= 0.5F; // Determines the animation speed. Higher value = faster animation.
+            npc.frameCounter %= Main.npcFrameCount[npc.type];
+            int frame = (int)npc.frameCounter;
+            npc.frame.Y = frame * frameHeight;
+
+            npc.spriteDirection = npc.direction;
+        }
+    }
+}
