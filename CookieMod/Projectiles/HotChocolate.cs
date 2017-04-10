006b039191b2aa36c1537f5ae6821cca55429393
CookieBiscuit <aidan.n.borne@gmail.com>
2017-04-10 18:11:57 -0400

    Update HotChocolate.cs

--- CookieMod/Projectiles/HotChocolate.cs
+++ CookieMod/Projectiles/HotChocolate.cs
@@ -10,37 +10,15 @@ namespace CookieMod.Items
     {
         public override void SetDefaults()
         {
-        item.name = "Hot Chocolate";
-        item.UseSound = SoundID.Item3;
-        item.useStyle = 2;
-        item.useTurn = true;
-        item.useAnimation = 17;
-        item.useTime = 17;
-        item.maxStack = 99;
-        item.width = 10;
-        item.height = 10;
-        item.buffType = 25;
-        item.buffTime = 7200;
-	item.consumable = true;
-        item.value = 100;
-        item.holdStyle = 1;
-        item.ammo = 353;
-        item.notAmmo = true;
-	item.shoot = mod.ProjectileType ("HotChocolate");
-        }
-	public override void HoldStyle(Player player)
-	{
-	  player.itemLocation.X = player.Center.X + (float) (8 * player.direction);
-          player.itemLocation.Y = player.MountedCenter.Y + 11f;
+        this.name = "Hot Chocolate";
+        this.width = 14;
+        this.height = 14;
+        this.aiStyle = 68;
+        this.friendly = true;
+        this.penetrate = 1;
+        this.alpha = (int) byte.MaxValue;
+        this.thrown = true;
+        this.noEnchantments = true;
 	}
-
-        public override void AddRecipes()
-        {
-            ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(null, "DoughBall", 6);
-	    recipe.AddTile(TileID.Furnaces);
-            recipe.SetResult(this);
-            recipe.AddRecipe();
-        }
     }
 }
