dbb60c20cae8053506abd134d009de592905290b
CookieBiscuit <aidan.n.borne@gmail.com>
2017-04-10 19:48:42 -0400

    Update WarmCozy.cs

--- CookieMod/Buffs/WarmCozy.cs
+++ CookieMod/Buffs/WarmCozy.cs
@@ -16,7 +16,7 @@ namespace CookieMod.Buffs
         public override void Update(Player player, ref int buffIndex)
         {                                             //
             player.AddBuff(mod.BuffType("WarmCozy"), 1); //this is an example of how to add your own buff
-            player.statDefense += 6f;  //
+            player.statDefense += 6;  //
       	    player.meleeDamage -= .10f;
 	    player.meleeSpeed -= .10f;
         }
