45f71f9664c5037f60970465779233640a1d98ea
DanewDr <danen1701@gmail.com>
2017-04-12 09:00:27 +1000

    Create Honeyed.cs

---
+++ CookieMod/Buffs/Honeyed.cs
@@ -0,0 +1,22 @@
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+ 
+namespace CookieMod.Buffs
+{
+    public class Honeyed : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            Main.buffNoTimeDisplay[Type] = true;
+            Main.buffName[this.Type] = "Honied";
+            Main.buffTip[this.Type] = "Decreases movementspeed";
+        }
+        public override void Update(Player player, ref int buffIndex)
+        {                                             //
+            player.AddBuff(mod.BuffType("Honyed"), 1); //this is an example of how to add your own buff
+            player.moveSpeed -= 0.50f;;  //
+        }
+    }
+}
