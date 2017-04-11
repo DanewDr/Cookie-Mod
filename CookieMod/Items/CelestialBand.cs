1cc0d52ac9427968485017eff691e2d0103487ec
CookieBiscuit <aidan.n.borne@gmail.com>
2017-04-11 16:54:33 -0400

    Update CelestialBand.cs

--- CookieMod/Items/CelestialBand.cs
+++ CookieMod/Items/CelestialBand.cs
@@ -8,31 +8,29 @@ namespace CookieMod.Items
 	{
 		public override void SetDefaults()
 		{
-			item.name = "Celestial Band";
+			item.name = "Luminite Band";
 			item.width = 30;
 			item.height = 20;
 			item.accessory = true;
 			item.toolTip = "The power of the gods flows through you";
 			item.rare = 10;
-			item.defense = 6;
+			item.defense = 4;
 		}
 		public override void UpdateAccessory(Player player, bool hideVisual)
 		{
-		player.moveSpeed += 2f;
+		player.moveSpeed *= 2f;
 		player.thrownDamage += .25f;
 		player.meleeDamage += .25f;
 		player.magicDamage += .25f;
 		player.rangedDamage += .25f;
-		player.meleeSpeed += .5f;
+		player.meleeSpeed += .10f;
 		}
 		public override void AddRecipes()
 		{
 			ModRecipe recipe = new ModRecipe(mod);
 			recipe.AddIngredient(null, "SpeedBand");
-			recipe.AddIngredient(ItemID.FragmentSolar, 20);
-			recipe.AddIngredient(ItemID.FragmentNebula, 20);
-			recipe.AddIngredient(ItemID.FragmentVortex, 20);
-			recipe.AddIngredient(ItemID.FragmentStardust, 20);
+			recipe.AddIngredient(ItemID.LunarBar, 10);
+			recipe.AddIngredient(ItemID.LunarOre);
 			recipe.AddTile(TileID.LunarCraftingStation);
 			recipe.SetResult(this);
 			recipe.AddRecipe();
