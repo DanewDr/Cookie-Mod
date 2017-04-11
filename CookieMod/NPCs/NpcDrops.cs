6da4941307ab8b6001a8f18d9fe20542f21f21dd
DanewDr <danen1701@gmail.com>
2017-04-11 10:31:15 +1000

    Update NpcDrops.cs

--- CookieMod/NPCs/NpcDrops.cs
+++ CookieMod/NPCs/NpcDrops.cs
@@ -23,6 +23,69 @@ namespace CookieMod.NPCs
                 }
                 CookieModWorld.spawnSugar = true;   //so the message and the ore spawn does not proc(show) when you kill EoC/npc again
             }
+            if (npc.type == NPCID.Bunny)   //this is where you choose the npc you want
+            {
+                if (Main.rand.Next(1) == 0) //this is the item rarity, so 4 is 1 in 5 chance that the npc will drop the item.
+                {
+                    {
+                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("BunnyFur"), Main.rand.Next(1, 5));
+                    }
+                }
+            }
+           if (npc.type == NPCID.BunnySlimed)   //this is where you choose the npc you want
+            {
+                if (Main.rand.Next(1) == 0) //this is the item rarity, so 4 is 1 in 5 chance that the npc will drop the item.
+                {
+                    {
+                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("BunnyFur"), Main.rand.Next(1, 5));
+                    }
+                }
+            }
+            if (npc.type == NPCID.BunnyXmas)   //this is where you choose the npc you want
+            {
+                if (Main.rand.Next(1) == 0) //this is the item rarity, so 4 is 1 in 5 chance that the npc will drop the item.
+                {
+                    {
+                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("BunnyFur"), Main.rand.Next(1, 5));
+                    }
+                }
+            }
+            if (npc.type == NPCID.GoldBunny)   //this is where you choose the npc you want
+            {
+                if (Main.rand.Next(1) == 0) //this is the item rarity, so 4 is 1 in 5 chance that the npc will drop the item.
+                {
+                    {
+                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("BunnyFur"), Main.rand.Next(10, 50));
+                    }
+                }
+            }
+            if (npc.type == NPCID.CrimsonBunny)   //this is where you choose the npc you want
+            {
+                if (Main.rand.Next(1) == 0) //this is the item rarity, so 4 is 1 in 5 chance that the npc will drop the item.
+                {
+                    {
+                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("BunnyFur"), Main.rand.Next(1, 5));
+                    }
+                }
+            }
+            if (npc.type == NPCID.PartyBunny)   //this is where you choose the npc you want
+            {
+                if (Main.rand.Next(1) == 0) //this is the item rarity, so 4 is 1 in 5 chance that the npc will drop the item.
+                {
+                    {
+                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("BunnyFur"), Main.rand.Next(5, 10));
+                    }
+                }
+            }
+            if (npc.type == NPCID.CorruptBunny)   //this is where you choose the npc you want
+            {
+                if (Main.rand.Next(1) == 0) //this is the item rarity, so 4 is 1 in 5 chance that the npc will drop the item.
+                {
+                    {
+                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("BunnyFur"), Main.rand.Next(5, 10));
+                    }
+                }
+            }
         }
     }
 }
