5f0c7037c740953aa0a622da7d104c1b05122c9a
DanewDr <danen1701@gmail.com>
2017-04-16 09:40:15 +1000

    Update NpcDrops.cs

--- CookieMod/NPCs/NpcDrops.cs
+++ CookieMod/NPCs/NpcDrops.cs
@@ -8,7 +8,22 @@ namespace CookieMod.NPCs
     {
         public override void NPCLoot(NPC npc)
         {
-
+            if (Main.player[Main.myPlayer].ZoneDungeon)          //this is where you choose what biome you whant the item to drop. ZoneCorrupt is in Corruption
+                {
+                    if (Main.rand.Next(9) == 0)      //this is the item rarity, so 9 = 1 in 10 chance that the npc will drop the item.
+                    {
+                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("GingerRoot"), Main.rand.Next(5, 10));//this is where you set what item to drop ,ItemID.VileMushroom is an example of how to add vanila items , Main.rand.Next(5, 10) it's the quantity,so it will chose a number from 5 to 10
+                    }
+                }
+            }
+            if (Main.player[Main.myPlayer].ZoneUnderworld
+                {
+                    if (Main.rand.Next(9) == 0)
+                    {
+                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("MoltenChocolate"), Mian.rand.Next(5, 15));
+                    }
+                }
+            }
             if (npc.type == NPCID.QueenBee)   
             {
                 if (Main.rand.Next(1) == 0) 
