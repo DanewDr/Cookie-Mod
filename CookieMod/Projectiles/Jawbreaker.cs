948b4393eeef449052b116022942b13ac374dc5b
CookieBiscuit <aidan.n.borne@gmail.com>
2017-04-15 15:16:41 -0400

    Create Jawbreaker.cs

---
+++ CookieMod/Projectiles/Jawbreaker.cs
@@ -0,0 +1,23 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CookieMod.Projectiles
+{
+    public class Jawbreaker : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.name = "Jawbreaker";
+            projectile.width = 8;
+            projectile.height = 8;
+            projectile.friendly = true;
+            projectile.ranged = true;
+            projectile.alpha = 0;
+            projectile.extraUpdates = 1;
+            aiType = ProjectileID.Bullet;
+        }       
+    }
+}
