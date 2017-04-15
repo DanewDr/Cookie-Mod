e64927b76377e40ceb970cc214d61a0a48b21cc2
CookieBiscuit <aidan.n.borne@gmail.com>
2017-04-15 14:33:59 -0400

    Create SugarPopper.cs

---
+++ CookieMod/Items/Weapons/SugarPopper.cs
@@ -0,0 +1,58 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Items.Weapons
+{
+	public class ExampleDualUseWeapon : ModItem
+	{
+		public override void SetDefaults()
+		{
+			item.name = "Sugar Popper";
+			item.damage = 20;
+			item.melee = true;
+			item.width = 20;
+			item.height = 40;
+			item.useTime = 13;
+			item.useAnimation = 13;
+			item.useStyle = 5;
+			item.knockBack = 4;
+			item.rare = 3;
+			item.UseSound = SoundID.Item5;
+			item.autoReuse = true;
+			item.shoot = ProjectileID.Arrow;
+			item.shootSpeed = 5f;
+      item.useAmmo = AmmoID.Arrow
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(null, "HardSugarDust", 15);
+			recipe.AddTile(null, "CookieWorkbench");
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+
+		public override bool AltFunctionUse(Player player)
+		{
+			return true;
+		}
+
+		public override bool CanUseItem(Player player)
+		{
+			if (player.altFunctionUse == 2)
+			{
+				item.shoot = ProjectileID.Bullet;
+        item.useAmmo = AmmoID.Bullet;
+			}
+			else
+			{
+        item.shoot = ProjectileID.Arrow;
+        item.useAmmo = AmmoID.Arrow;
+			}
+			return base.CanUseItem(player);
+		}
+	}
+}
