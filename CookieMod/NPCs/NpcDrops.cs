0dfef6f1a31ca1a31932859fcf17de98b128f0e7
DanewDr <danen1701@gmail.com>
2017-04-16 11:14:20 +1000

    Update NpcDrops.cs

--- CookieMod/NPCs/NpcDrops.cs
+++ CookieMod/NPCs/NpcDrops.cs
@@ -7,22 +7,48 @@ namespace CookieMod.NPCs
     public class NpcDrops : GlobalNPC
     {
         public override void NPCLoot(NPC npc)
-        {
-            if (Main.player[Main.myPlayer].ZoneDungeon)          //this is where you choose what biome you whant the item to drop. ZoneCorrupt is in Corruption
+     	{		
+                if (Main.hardMode)     //this make the item drop only in hardmode
                 {
-                    if (Main.rand.Next(9) == 0)      //this is the item rarity, so 9 = 1 in 10 chance that the npc will drop the item.
+				    if (Main.player[Main.myPlayer].ZoneDungeon)          //this is where you choose what biome you whant the item to drop. ZoneCorrupt is in Corruption
+					    {
+						    if (Main.rand.Next(9) == 0)      //this is the item rarity, so 9 = 1 in 10 chance that the npc will drop the item.
+						    {
+							    Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("GingerRoot"), Main.rand.Next(5, 10));//this is where you set what item to drop ,ItemID.VileMushroom is an example of how to add vanila items , Main.rand.Next(5, 10) it's the quantity,so it will chose a number from 5 to 10
+						    }
+					    }
+				    }
+			    else    //else if it's not hardmode this will happen
+                {
+                    if (Main.player[Main.myPlayer].ZoneDungeon)  //so again if the player is in corruption
                     {
-                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("GingerRoot"), Main.rand.Next(5, 10));//this is where you set what item to drop ,ItemID.VileMushroom is an example of how to add vanila items , Main.rand.Next(5, 10) it's the quantity,so it will chose a number from 5 to 10
+                        if (Main.rand.Next(2) == 0)    //   the item has a 1 in 3 chance to drop
+                        {
+                            Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("GingerRoot"), Main.rand.Next(1, 5));
+                        }
                     }
-                }
+ 
             }
-            if (Main.player[Main.myPlayer].ZoneUnderworld
+			if (Main.hardMode)     
+            {
+				if (Main.player[Main.myPlayer].ZoneUnderworldHeight)          
+					{
+						if (Main.rand.Next(9) == 0)      
+						{
+							Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("MoltenChocolate"), Main.rand.Next(1, 5));
+						}
+					}
+				}
+			else    
+            {
+                if (Main.player[Main.myPlayer].ZoneUnderworldHeight)  
                 {
-                    if (Main.rand.Next(9) == 0)
+                    if (Main.rand.Next(2) == 0)    
                     {
-                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("MoltenChocolate"), Mian.rand.Next(5, 15));
+                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("MoltenChocolate"), Main.rand.Next(1, 2));
                     }
                 }
+ 
             }
             if (npc.type == NPCID.QueenBee)   
             {
