b5db6a362f01d12404e0436e16274b534b19ec4c
DanewDr <danen1701@gmail.com>
2017-06-29 17:20:57 +1000

    Update GingerRoot.cs

--- CookieMod/Items/GingerRoot.cs
+++ CookieMod/Items/GingerRoot.cs
@@ -6,14 +6,17 @@ namespace CookieMod.Items
 {
 	public class GingerRoot : ModItem
 	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Ginger Root");
+			Tooltip.SetDefault("Mmmh");
+		}
 		public override void SetDefaults()
 		{
-			item.name = "Ginger Root";
 			item.width = 16;
 			item.height = 16;
 			item.maxStack = 999;
 			item.rare = 3;
-			item.toolTip = "Mmmh.";
 		}
 	}
 }
