aae5a2eb72b8e73eb2c835b29177779773d06edd
DanewDr <danen1701@gmail.com>
2017-06-29 18:45:41 +1000

    Update HardSugarball.cs

--- CookieMod/Items/Weapons/HardSugarball.cs
+++ CookieMod/Items/Weapons/HardSugarball.cs
@@ -6,15 +6,18 @@ namespace CookieMod.Items.Weapons
 {
 	public class HardSugarball : ModItem
 	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Hardned Sugarball");
+			Tooltip.SetDefault("This packs quite a punch");
+		}
 		public override void SetDefaults()
 		{
 			item.CloneDefaults(ItemID.Snowball);
-			item.name = "Hardened Sugarball";
 			item.damage = 25;
 			item.width = 12;
 			item.height = 12;
 			item.maxStack = 999;
-			item.toolTip = "This packs quite a punch.";
 			item.rare = 3;
 			item.consumable = true;
 			item.knockBack = 8f;
