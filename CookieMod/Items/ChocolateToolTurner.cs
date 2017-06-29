0cc877d7c5f071382c9e8aab2961546def4cdb14
DanewDr <danen1701@gmail.com>
2017-06-29 17:09:31 +1000

    Update ChocolateToolTurner.cs

--- CookieMod/Items/ChocolateToolTurner.cs
+++ CookieMod/Items/ChocolateToolTurner.cs
@@ -6,28 +6,23 @@ namespace CookieMod.Items
 {
 	public class ChocolateToolTurner : ModItem
 	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Chocolate Tool Turner");
+		}
 		public override void SetDefaults()
 		{
-			item.name = "Chocolate Tool Turner";
 			item.width = 24;
 			item.height = 28;
-			item.toolTip = "This is a modded accessory.";
 			item.value = 10000;
 			item.rare = 2;
 			item.accessory = true;
-			item.defense = 1000;
 			item.lifeRegen = 19;
 		}
 
 		public override void UpdateAccessory(Player player, bool hideVisual)
 		{
-			player.statDefense = 0;
-			player.meleeDamage = 0.1f;
-			player.thrownDamage = 0.1f;
-			player.rangedDamage = 0.1f;
-			player.magicDamage = 0.1f;
-			player.minionDamage = 0.1f;
-			player.lifeRegen = -120;
+			player.statDefense = 1;
 		}
 
 		public override void AddRecipes()
