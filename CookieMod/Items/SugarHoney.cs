55788605618cb285f7b6444d350b78a0dee3a69c
DanewDr <danen1701@gmail.com>
2017-06-29 17:31:21 +1000

    Update SugarHoney.cs

--- CookieMod/Items/SugarHoney.cs
+++ CookieMod/Items/SugarHoney.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -7,10 +7,14 @@ namespace CookieMod.Items
 {
     public class SugarHoney : ModItem
     {
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Artificial Honey");
+			Tooltip.SetDefault("");
+		}
         public override void SetDefaults()
         {
             item.CloneDefaults(ItemID.HealingPotion);
-            item.name = "Artificial Honey";
         }
     }
 }
