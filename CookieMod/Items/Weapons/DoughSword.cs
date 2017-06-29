7ef89b58f0ebff2366ae8d8f2ff8574239230bab
DanewDr <danen1701@gmail.com>
2017-06-29 18:36:27 +1000

    Update DoughSword.cs

--- CookieMod/Items/Weapons/DoughSword.cs
+++ CookieMod/Items/Weapons/DoughSword.cs
@@ -10,14 +10,17 @@ namespace CookieMod.Items.Weapons   //where is located
 {
     public class DoughSword : ModItem
     {
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Dough Blade");
+			Tooltip.SetDefault("Dough rolled onto a wooden sword.");
+		}
         public override void SetDefaults()
         {
-            item.name = "Dough Blade";     //Sword name
             item.damage = 9;            //Sword damage
             item.melee = true;            //if it's melee
             item.width = 30;              //Sword width
             item.height = 30;             //Sword height
-            item.toolTip = "Dough rolled onto a wooden sword.";  //Item Description
             item.useTime = 24;          //how fast 
             item.useAnimation = 24;     
             item.useStyle = 1;        //Style is how this item is used, 1 is the style of the sword
