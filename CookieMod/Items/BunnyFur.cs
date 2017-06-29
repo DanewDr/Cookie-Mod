99e2e11921628abffd3e34a76fb9399ac5efe8df
DanewDr <danen1701@gmail.com>
2017-06-29 17:03:55 +1000

    Update BunnyFur.cs

--- CookieMod/Items/BunnyFur.cs
+++ CookieMod/Items/BunnyFur.cs
@@ -7,12 +7,16 @@ namespace CookieMod.Items
 {
     public class BunnyFur : ModItem
     {
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Bunny Fur");
+			Tooltip.SetDefault("Why you kill bunny?");
+		}
+
         public override void SetDefaults()
         {
-            item.name = "Bunny Fur";
             item.width = 20;
             item.height = 20;
-            item.toolTip = "Why you kill bunny?";
             item.value = 10;
             item.rare = 1;
             item.maxStack = 999;
