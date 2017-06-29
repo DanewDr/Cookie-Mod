58239296fe3dc6b9f63b90390776274057b96383
DanewDr <danen1701@gmail.com>
2017-06-29 17:16:19 +1000

    Update DoughBall.cs

--- CookieMod/Items/DoughBall.cs
+++ CookieMod/Items/DoughBall.cs
@@ -7,12 +7,15 @@ namespace CookieMod.Items
 {
     public class DoughBall : ModItem
     {
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Dough Ball");
+			Tooltip.SetDefault("Used to craft cookies");
+		}
         public override void SetDefaults()
         {
-            item.name = "Dough Ball";
             item.width = 20;
             item.height = 20;
-            item.toolTip = "Used to craft cookies";
             item.value = 100;
             item.rare = 1;
             item.maxStack = 999;
