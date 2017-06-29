999a9569e9b38f1bedbcb2f0c1c5e1db261bf165
DanewDr <danen1701@gmail.com>
2017-06-29 18:56:15 +1000

    Update RainbowPopsicle.cs

--- CookieMod/Items/Weapons/RainbowPopsicle.cs
+++ CookieMod/Items/Weapons/RainbowPopsicle.cs
@@ -6,9 +6,13 @@ namespace CookieMod.Items.Weapons
 {
 	public class RainbowPopsicle : ModItem
 	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Rainbow Popsicle");
+			Tooltip.SetDefault("");
+		}
 		public override void SetDefaults()
 		{
-			item.name = "Rainbow Popsicle";
 			item.width = 12;
 			item.height = 12;
 			item.maxStack = 999;
