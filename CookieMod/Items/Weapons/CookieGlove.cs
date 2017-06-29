5d8766a665d3866b4d1ea28ecf534d2ceea3c823
DanewDr <danen1701@gmail.com>
2017-06-29 18:30:06 +1000

    Update CookieGlove.cs

--- CookieMod/Items/Weapons/CookieGlove.cs
+++ CookieMod/Items/Weapons/CookieGlove.cs
@@ -7,15 +7,18 @@ namespace CookieMod.Items.Weapons
 {
 	public class CookieGlove : ModItem
 	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Cookie Tosser");
+			Tooltip.SetDefault("Throws cookies");
+		}
 		public override void SetDefaults()
 		{
-			item.name = "Cookie Tosser";
 			item.damage = 18;
 			item.thrown = true;
 			item.noMelee = true;
 			item.width = 34;
 			item.height = 22;
-			item.toolTip = "Throws cookies.";
 			item.rare = 2;
 			item.useTime = 25;
 			item.useAnimation = 10;
