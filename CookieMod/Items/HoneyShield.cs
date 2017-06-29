d6ec1a0a550bf42b4576947dc7ffba0e21317a0a
DanewDr <danen1701@gmail.com>
2017-06-29 16:27:30 +1000

    Update HoneyShield.cs

--- CookieMod/Items/HoneyShield.cs
+++ CookieMod/Items/HoneyShield.cs
@@ -1,26 +1,21 @@
-using System;
-using System.Collections.Generic;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
  
 namespace CookieMod.Items     //We need this to basically indicate the folder where it is to be read from, so you the texture will load correctly
 {
-    public class HoneyShield : ModItem
-    {
-        public override bool Autoload(ref string name, ref string texture, IList<EquipType> equips)
-        {
-            equips.Add(EquipType.Shield);  //this auotoload the shield texture sheet on player
-            return true;
-        }
- 
+	[AutoloadEquip(EquipType.Shield)]
+	public class HoneyShield : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Honey Shield");
+			Tooltip.SetDefault("Grants immunity to knckback\n8% magic and summoner damage increase");
+		}		
         public override void SetDefaults()
         {
-            item.name = "Honey Shield";      //the name displayed when hovering over the Weapon ingame.
             item.width = 24;   //The size in width of the sprite in pixels.
             item.height = 28;    //The size in height of the sprite in pixels.
-            item.toolTip = "Grants immunity to knockback.";
-	    item.toolTip2 = "8% magic and summoner damage increase";	//The description of the Weapon shown when hovering over the item ingame.
             item.value = Item.buyPrice(0, 2, 0, 0); //  How much the item is worth, in copper coins, when you sell it to a merchant. It costs 1/5th of this to buy it back from them. An easy way to remember the value is platinum, gold, silver, copper or PPGGSSCC (so this item price is 2 gold)
             item.rare = 4;          //The color the title of your Weapon when hovering over it ingame        
             item.accessory = true;  //this make the item an accessory, so you can equip it
