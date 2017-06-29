c55c9f5e193cc4655b41ac9bd2e3f430048406f4
DanewDr <danen1701@gmail.com>
2017-06-29 18:58:57 +1000

    Update SugarSharkFin.cs

--- CookieMod/Items/Weapons/SugarSharkFin.cs
+++ CookieMod/Items/Weapons/SugarSharkFin.cs
@@ -6,15 +6,18 @@ namespace CookieMod.Items.Weapons
 {
 	public class SugarSharkFin : ModItem
 	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Sugar Shark Fin");
+			Tooltip.SetDefault("This is shark cruelty");
+		}
 		public override void SetDefaults()
 		{
-			item.name = "Sugar Shark Fin";
 			item.width = 12;
 			item.height = 12;
 			item.maxStack = 999;
 			item.damage = 3;
 			item.knockBack = 4f;
-			item.toolTip = "This is shark cruelty";
 			item.rare = 2;
 			item.consumable = true;
 			item.ammo = item.type;
