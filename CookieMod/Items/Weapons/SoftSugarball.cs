f462b1e2caf50997ef209701f3ba21fae6563071
DanewDr <danen1701@gmail.com>
2017-06-29 18:57:18 +1000

    Update SoftSugarball.cs

--- CookieMod/Items/Weapons/SoftSugarball.cs
+++ CookieMod/Items/Weapons/SoftSugarball.cs
@@ -6,15 +6,18 @@ namespace CookieMod.Items.Weapons
 {
 	public class SoftSugarball : ModItem
 	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Sugarball");
+			Tooltip.SetDefault("Hurts quite a bit actually");
+		}
 		public override void SetDefaults()
 		{
 			item.CloneDefaults(ItemID.Snowball);
-			item.name = "Sugarball";
 			item.damage = 11;
 			item.width = 12;
 			item.height = 12;
 			item.maxStack = 999;
-			item.toolTip = "Hurts quite a bit actually.";
 			item.rare = 2;
 			item.consumable = true;
 			item.knockBack = 1f;
