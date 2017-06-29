f7083f1cfd51c2dc86f88d0ba592082f9e03a1d0
DanewDr <danen1701@gmail.com>
2017-06-29 17:14:08 +1000

    Update CorruptCookieTreasurebag.cs

--- CookieMod/Items/CorruptCookieTreasurebag.cs
+++ CookieMod/Items/CorruptCookieTreasurebag.cs
@@ -10,14 +10,17 @@ namespace CookieMod.Items
 {
     public class CorruptCookieTreasurebag : ModItem
     {
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Treasure Bag");
+			Tooltip.SetDefault("Right click to open");
+		}
         public override void SetDefaults()
         {
-            item.name = "Treasure Bag";
             item.maxStack = 999;
             item.consumable = true;
             item.width = 24;
             item.height = 24;
-            item.toolTip = "Right click to open";
             item.rare = 11;
             bossBagNPC = mod.NPCType("CorruptCookie1");  //add this if the tresure bag is droped from a boss
             item.expert = true;      //add this if it's expert mode only
