8a3aeb77d35b85b58009dae5e36e0c4d54263b78
DanewDr <danen1701@gmail.com>
2017-06-29 17:31:46 +1000

    Update SugarWater.cs

--- CookieMod/Items/SugarWater.cs
+++ CookieMod/Items/SugarWater.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -7,10 +7,14 @@ namespace CookieMod.Items
 {
     public class SugarWater : ModItem
     {
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Sugar Water");
+			Tooltip.SetDefault("");
+		}
         public override void SetDefaults()
         {
             item.CloneDefaults(ItemID.LesserHealingPotion);
-            item.name = "Sugar Water";
         }
     }
 }
