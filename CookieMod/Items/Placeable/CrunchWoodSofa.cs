ab617d484fba1bb9aa02aaccaaa6f69ef4851fff
DanewDr <danen1701@gmail.com>
2017-06-29 17:38:14 +1000

    Update CrunchWoodSofa.cs

--- CookieMod/Items/Placeable/CrunchWoodSofa.cs
+++ CookieMod/Items/Placeable/CrunchWoodSofa.cs
@@ -3,29 +3,32 @@ using Terraria.ModLoader;
 
 namespace CookieMod.Items.Placeable
 {
-	public class CrunchWoodSofa : ModItem
+	public class CrunchWoodDresser: ModItem
 	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Crunch Dresser");
+			Tooltip.SetDefault("Change clothes and even stor items!");
+		}
 		public override void SetDefaults()
 		{
-			item.name = "Crunch Sofa";
-			item.width = 12;
-			item.height = 30;
+			item.width = 26;
+			item.height = 22;
 			item.maxStack = 99;
-			AddTooltip("Don't think it will be comfortable to sit on");
 			item.useTurn = true;
 			item.autoReuse = true;
 			item.useAnimation = 15;
 			item.useTime = 10;
 			item.useStyle = 1;
 			item.consumable = true;
-			item.value = 150;
-			item.createTile = mod.TileType("CrunchWoodSofa");
+			item.value = 500;
+			item.createTile = mod.TileType("CrunchWoodDresser");
 		}
 
 		public override void AddRecipes()
 		{
 			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ItemID.Silk, 2);
+			recipe.AddIngredient(ItemID.Dresser);
 			recipe.AddIngredient(null, "CrunchWood", 10);
 			recipe.SetResult(this);
 			recipe.AddRecipe();
