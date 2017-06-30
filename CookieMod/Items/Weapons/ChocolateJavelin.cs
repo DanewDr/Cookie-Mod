144e842440cd2e4248d5baf8422e724d91cdee0d
DanewDr <danen1701@gmail.com>
2017-06-30 21:04:22 +1000

    Update ChocolateJavelin.cs

--- CookieMod/Items/Weapons/ChocolateJavelin.cs
+++ CookieMod/Items/Weapons/ChocolateJavelin.cs
@@ -10,10 +10,14 @@ namespace CookieMod.Items.Weapons
 {
 	public class ChocolateJavelin : ModItem
 	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Chocolate Javelin");
+			Tooltip.SetDefault("");
+		}
 		public override void SetDefaults()
 		{
 			// Alter any of these values as you see fit, but you should probably keep useStyle on 1, as well as the noUseGraphic and noMelee bools
-			item.name = "Chocolate Javelin";
 			item.shoot = mod.ProjectileType<Projectiles.ChocolateJavelinProjectile>(); // Notice a newer way to get projectile types, much cleaner than using strings!
 			item.shootSpeed = 10f;
 			item.damage = 26;
