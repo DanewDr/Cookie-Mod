191d75bca2c2cf436a604d4fccc3f6ad0749f3e7
DanewDr <danen1701@gmail.com>
2017-06-30 21:40:49 +1000

    Update SuperSwirl.cs

--- CookieMod/Projectiles/SuperSwirl.cs
+++ CookieMod/Projectiles/SuperSwirl.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
@@ -9,9 +9,12 @@ namespace CookieMod.Projectiles
 {
     public class SuperSwirl : ModProjectile
     {
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Sweetnir Swirl");     //The English name of the projectile
+		}
 		public override void SetDefaults()
 		{
-			projectile.name = "Sweetnir Swirl";
 			aiType = ProjectileID.NightBeam;
 			projectile.melee = true;
 			projectile.friendly = true;
