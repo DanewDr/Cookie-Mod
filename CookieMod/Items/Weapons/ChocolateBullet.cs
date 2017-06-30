a89e71d9346f6289ea5fb7321a04359e6ee49530
DanewDr <danen1701@gmail.com>
2017-06-30 21:02:43 +1000

    Update ChocolateBullet.cs

--- CookieMod/Items/Weapons/ChocolateBullet.cs
+++ CookieMod/Items/Weapons/ChocolateBullet.cs
@@ -1,36 +1,39 @@
-using System;
-using Microsoft.Xna.Framework;
-using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
- 
+
 namespace CookieMod.Items.Weapons
 {
-    public class ChocolateBlast : ModItem
-    {
+	public class ChocolateBullet : ModItem
+	{
 		public override void SetStaticDefaults()
 		{
-			DisplayName.SetDefault("Chocolate Blast");
-			Tooltip.SetDefault("Unleash the power of chocolate cookies");
+			DisplayName.SetDefault("Chocolate Bullet");
+			Tooltip.SetDefault("");
 		}
-        public override void SetDefaults()
-        {
-            item.damage = 32;                        
-            item.magic = true;                     //this make the item do magic 
-            item.width = 24;
-            item.height = 28;
-            item.useTime = 15;
-            item.useAnimation = 15;
-            item.useStyle = 5;        //this is how the item is holded
-            item.noMelee = true;
-            item.knockBack = 10;        
-            item.value = 1000;
-            item.rare = 4;
-            item.mana = 6;             //mana use
-            item.UseSound = SoundID.Item21;            //this is the 
-            item.autoReuse = true;
-            item.shoot = mod.ProjectileType ("GiantCookie");  //this make the item shoot 
-            item.shootSpeed = 8f;    //projectile speed when shoot
-        }      
-    }
+		public override void SetDefaults()
+		{
+			item.damage = 12;
+			item.ranged = true;
+			item.width = 8;
+			item.height = 8;
+			item.maxStack = 999;
+			item.consumable = true;             //You need to set the item consumable so that the ammo would automatically consumed
+			item.knockBack = 1.5f;
+			item.value = 10;
+			item.rare = 2;
+			item.shoot = mod.ProjectileType("ChocolateBullet");   //The projectile shoot when your weapon using this ammo
+			item.shootSpeed = 16f;                  //The speed of the projectile
+			item.ammo = AmmoID.Bullet;              //The ammo class this ammo belongs to.
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.MusketBall, 50);
+			recipe.AddIngredient(null, "ChocolateCookie", 1);
+			recipe.AddTile(null, "CookieWorkbench");
+			recipe.SetResult(this, 50);
+			recipe.AddRecipe();
+		}
+	}
 }
