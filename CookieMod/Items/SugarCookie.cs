06499464daaf1fe7f522ec6f337ac7a2de4c7c56
CookieBiscuit <aidan.n.borne@gmail.com>
2017-07-27 15:30:46 -0500

    Create SugarCookie.cs

---
+++ CookieMod/Items/SugarCookie.cs
@@ -0,0 +1,38 @@
+using Microsoft.Xna.Framework;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+ 
+namespace CookieMod.Items
+{
+    public class SugarCookie : ModItem
+    {
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Sugar Cookie");
+			Tooltip.SetDefault("Warm and toasty");
+		}
+
+        public override void SetDefaults()
+        {
+            item.damage = 14;
+	    item.consumable = true;
+	    item.shoot = mod.ProjectileType ("ThrowingCookieSugar");
+	    item.ammo = mod.ItemType("SlingshotAmmo");
+	    item.width = 20;
+            item.height = 20;
+            item.value = 100;
+            item.rare = 1;
+            item.maxStack = 999;
+	}   
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(null, "Cookie", 5);
+	    recipe.AddIngredient(null, "SugarDust", 3);
+	    recipe.AddTile(null, "CookieWorkbench");
+	    recipe.SetResult(this, 5);
+            recipe.AddRecipe();
+        }
+    }
+}
