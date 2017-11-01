c8500aaf59c8d8b997ff1614b89fad9bb636d641
CookieBiscuit <aidan.n.borne@gmail.com>
2017-10-31 23:22:40 -0500

    Update SugarBeam.cs

--- CookieMod/Projectiles/SugarBeam.cs
+++ CookieMod/Projectiles/SugarBeam.cs
@@ -15,8 +15,15 @@ namespace CookieMod.Projectiles
 		}
 		public override void SetDefaults()
 		{
-			projectile.CloneDefaults(ProjectileID.DiamondBolt);
-			aiType = ProjectileID.DiamondBolt;
+			aiType = ProjectileID.Bullet;
+			projectile.CloneDefaults(ProjectileID.Bullet);
+			projectile.friendly = true;
+			projectile.tileCollide = true;
+			projectile.ignoreWater = false;
+			projectile.penetrate = 1;
+			projectile.width = 8;
+			projectile.height = 4;
+			projectile.extraUpdates = 1;
 		}
 
 
