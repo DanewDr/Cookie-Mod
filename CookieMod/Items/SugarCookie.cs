f0657169885c51201434efccc1140e3378a9466e
DanewDr <danen1701@gmail.com>
2017-11-04 13:19:32 +1100

    Update SugarCookie.cs

--- CookieMod/Items/SugarCookie.cs
+++ CookieMod/Items/SugarCookie.cs
@@ -22,7 +22,7 @@ namespace CookieMod.Items
 	    item.width = 20;
             item.height = 20;
             item.value = 100;
-            item.rare = 1;
+            item.rare = 2;
             item.maxStack = 999;
 	}   
         public override void AddRecipes()
