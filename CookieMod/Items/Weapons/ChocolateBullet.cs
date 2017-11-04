935055044b3011bd7e77d149e66ce2409cd6966b
DanewDr <danen1701@gmail.com>
2017-11-04 12:57:40 +1100

    Update ChocolateBullet.cs

--- CookieMod/Items/Weapons/ChocolateBullet.cs
+++ CookieMod/Items/Weapons/ChocolateBullet.cs
@@ -20,7 +20,7 @@ namespace CookieMod.Items.Weapons
 			item.consumable = true;             //You need to set the item consumable so that the ammo would automatically consumed
 			item.knockBack = 1.5f;
 			item.value = 10;
-			item.rare = 2;
+			item.rare = 3;
 			item.shoot = mod.ProjectileType("ChocolateBullet");   //The projectile shoot when your weapon using this ammo
 			item.shootSpeed = 16f;                  //The speed of the projectile
 			item.ammo = AmmoID.Bullet;              //The ammo class this ammo belongs to.
