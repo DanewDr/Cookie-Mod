1218dbd93fa604fdff2d5a2b5a23c0194740420a
DanewDr <danen1701@gmail.com>
2018-03-19 20:32:59 +1100

    Add files via upload

--- CookieMod/Items/ChocolateCookie.cs
+++ CookieMod/Items/ChocolateCookie.cs
@@ -2,10 +2,14 @@ using Microsoft.Xna.Framework;
 using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
  
 namespace CookieMod.Items
 {
-    public class ChocolateCookie : ModItem
+    public class ChocolateCookie : CookClass
     {
 		public override void SetStaticDefaults()
 		{
@@ -13,7 +17,7 @@ namespace CookieMod.Items
 			Tooltip.SetDefault("Not recommended for dogs");
 		}
 
-        public override void SetDefaults()
+        public override void SafeSetDefaults()
         {
             item.damage = 14;
 	    item.consumable = true;
