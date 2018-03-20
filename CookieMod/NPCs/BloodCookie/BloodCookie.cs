19e4b971c40fe83e588d40032143f8ac0b812c4a
DanewDr <danen1701@gmail.com>
2018-03-20 16:11:33 +1100

    Add files via upload

--- CookieMod/NPCs/BloodCookie/BloodCookie.cs
+++ CookieMod/NPCs/BloodCookie/BloodCookie.cs
@@ -58,9 +58,11 @@ namespace CookieMod.NPCs.BloodCookie
 				Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("Cookie"), Main.rand.Next(15, 25));
 				Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("Milk"), Main.rand.Next(6, 10));
 			}
-			CookieModWorld.downedCookieBoss = true;
-		}
-		
+			if (!CookieModWorld.downedCookieBoss)
+			{	
+				CookieModWorld.downedCookieBoss = true;
+			}
+		}		
 		public override void BossLoot(ref string name, ref int potionType)
 		{
 			name = "Blood Cookie";
