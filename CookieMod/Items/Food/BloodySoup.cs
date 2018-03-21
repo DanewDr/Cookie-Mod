07331d98ed9e3f6aafdc75e1f6491a054187b210
CookieBiscuit <aidan.n.borne@gmail.com>
2018-03-21 09:07:05 -0500

    Create BloodySoup.cs

---
+++ CookieMod/Items/Food/BloodySoup.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CookieMod.Items.Food
+{
+	public class BloodySoup : CookClass
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Bowl of Blood Soup");
+			Tooltip.SetDefault("Gives food poisoning");
+		}
+		public override void SafeSetDefaults()
+		{
+			item.width = 22;
+			item.height = 14;
+      item.damage = 16
+      item.crit = 4
+      item.useSound = SoundID.Item3;
+			item.maxStack = 30;
+			item.rare = 1;
+			item.consumable = true;
+      item.value = 400;
+			item.shoot = mod.ProjectileType ("BloodySoup");
+			item.ammo = mod.ItemType("BowlofSoup");
+		}
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Bowl);
+			recipe.AddIngredient(ItemID.ViciousMushroom);
+      recipe.AddIngredient(ItemID.Vertebrae);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
