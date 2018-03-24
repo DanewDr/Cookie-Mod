fc559316648a640dce2096de5f13d07ecf195d9c
DanewDr <danen1701@gmail.com>
2018-03-24 22:40:31 +1100

    Shouldn't last forever now

--- CookieMod/Buffs/FoodPoisoning.cs
+++ CookieMod/Buffs/FoodPoisoning.cs
@@ -18,7 +18,6 @@ namespace CookieMod.Buffs
         }
         public override void Update(Player player, ref int buffIndex)
         {                                             //
-            player.AddBuff(mod.BuffType("FoodPoisoning"), 1); //this is an example of how to add your own buff
 	    player.statDefense -= 2;
 	    player.meleeCrit -= 2;
 	    player.rangedCrit -= 2;
