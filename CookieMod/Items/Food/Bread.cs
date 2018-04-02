e367d2f8b067fb9f10f297e219579622acbe1de3
CookieBiscuit <aidan.n.borne@gmail.com>
2018-04-02 10:53:14 -0500

    Create Bread.cs

---
+++ CookieMod/Items/Food/Bread.cs
@@ -0,0 +1,39 @@
+using Microsoft.Xna.Framework;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ 
+namespace CookieMod.Items.Food
+{
+    public class Bread : CookClass
+    {
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Bread");
+			Tooltip.SetDefault("Not for the ducks!");
+		}
+        public override void SafeSetDefaults()
+        {
+            item.width = 26;
+            item.height = 24;
+            item.damage = 12
+            item.value = 100;
+            item.rare = 1;
+            item.maxStack = 999;
+			      item.consumable = true;
+			      item.ammo = item.type;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(null, "DoughBall", 3);
+			      recipe.AddTile(TileID.Furnaces);
+            recipe.SetResult(this, 6);
+            recipe.AddRecipe();
+        }
+    }
+}
